544740e6c4671167db53cb462d98d76087904f67
N.Saaliev <kenshin.nakamura@mail.ru>
2020-04-12 15:39:22 +0600

    Updates

---
+++ precache-manifest.f93efcec0cf1b9228cbc084315cdc337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b597c1c7c513d384572506b0fb3014",
+    "url": "/react-shello-app/index.html"
+  },
+  {
+    "revision": "97640d0ca6c8d506db5f",
+    "url": "/react-shello-app/static/css/main.6e6a43a3.chunk.css"
+  },
+  {
+    "revision": "af0a0e8bc9dfa05a2f57",
+    "url": "/react-shello-app/static/js/2.96edb81e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-shello-app/static/js/2.96edb81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97640d0ca6c8d506db5f",
+    "url": "/react-shello-app/static/js/main.88f57a56.chunk.js"
+  },
+  {
+    "revision": "89da4cee121d6cfdd806",
+    "url": "/react-shello-app/static/js/runtime-main.b7d17c43.js"
+  }
+]);
